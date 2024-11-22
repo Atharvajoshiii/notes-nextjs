2c6a7d7cf81dcb987280caa9172bde0c64e193bb
“Atharva <atharvajoshi814@gmail.com>
2024-11-22 23:28:15 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enable React strict mode
+  env: {
+    MONGODB_URL: process.env.MONGODB_URL, // Expose the environment variable
+  },
+  webpack: (config) => {
+    // Any custom Webpack configurations
+    return config;
+  },
 };
 
 export default nextConfig;
